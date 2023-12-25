aa65c313b655c223a16b4ba813a3d097aeb9fe71
Oishee12 <138649009+Oishee12@users.noreply.github.com>
2023-12-25 11:20:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+/* confusing code (not recommended)
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+*/
+
+// strict check
+console.log("2" === 2); // false
