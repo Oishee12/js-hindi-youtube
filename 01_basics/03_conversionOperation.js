96fe3c048ce1ec76a0c345a8f476e6942523bee4
Oishee12 <138649009+Oishee12@users.noreply.github.com>
2023-12-25 11:04:40 +0000

    Operations in datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33"
+/*let score = "33"
 
 console.log(typeof score); // score ki dhoroner datatype sheta dekhar jonno 
 console.log(typeof (score)); // score ki dhoroner datatype sheta dekhar jonno
@@ -8,18 +8,18 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 // null to number
-let scor = null
-console.log(typeof scor);
-let valueInNmber = Number(scor)
-console.log(typeof valueInNmber);
-console.log(valueInNmber);
+let score1 = null
+console.log(typeof score1);
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
 
 // string to number
-let scor ="Hitesh"
-console.log(typeof scor);
-let valueInNmber = Number(scor)
-console.log(typeof valueInNmber);
-console.log(valueInNmber);
+let score2 ="Hitesh"
+console.log(typeof score2);
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
 
 //anything => number conversion
 //33 => 33
@@ -37,3 +37,41 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "Hitesh" => true
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+*/
+
+
+// ****** Operations *******
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3); //2 to the power 3
+//console.log(2/2); 
+//console.log(2%2); //reminder
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let gameCounter
+gameCounter++
+console.log(gameCounter);
+
+
+// link for read
+//javascript documentation = https://tc39.es/ecma262/2023/
+//for search = prefix and postfix js mdn
