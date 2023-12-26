da46ebfb226c9f64cb40e88298b9afb645785c64
Oishee12 <138649009+Oishee12@users.noreply.github.com>
2023-12-26 13:16:02 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100) //new Number bolar karone output a datatype show hoiche
+// console.log(balance);
+
+// console.log(balance.toString()); //string a convert hoyeche
+
+// console.log(balance.toFixed(2)); //decimal place fixed
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));//shuru theke 3 ta number show korbe
+
+// const otherNumber1 = 123.8966
+// console.log(otherNumber1.toPrecision(3));//shuru theke 3 ta number show korbe
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // boro songkha'r majhe koma dey . *toLocaleString() => American standard
+// console.log(hundreds.toLocaleString('en-IN')); // boro songkha'r majhe koma dey . *toLocaleString('en-IN') => Indian standard
+
+
+
+//********MATHS**************** */
+console.log(Math);
+console.log(Math.abs(-4)); // abs shongkha k positive banay
+console.log(Math.round(4.3)); // 4.3 thakle er round figure hobe 4. 
+console.log(Math.round(4.7)); //  4.7 hole er round figure hobe 5 
+console.log(Math.ceil(4.2)); 
+console.log(Math.floor(4.9)); 
+console.log(Math.min(3, 4, -5, 1));
+console.log(Math.max(3, 4, -5, 1));
+
+
